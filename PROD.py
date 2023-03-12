30368dee651f11715005f0429ca50cc59704075d
Danil Golubintsev <danilmpower@gmail.com>
2023-03-12 17:17:38 +0300

    added comments to PROD.py

--- PROD.py
+++ PROD.py
@@ -5,11 +5,11 @@ from flask import Flask
 from flask_caching import Cache
 
 
-def get_ip():
-    IP = 0
-    while not IP:
+def get_ip():           # get IP from API
+    IP = 0  
+    while not IP:       # try getting IP until get
         try:
-            IP = json.loads(requests.get('https://api.ipify.org?format=json').text)['ip']   
+            IP = json.loads(requests.get('https://api.ipify.org?format=json').text)['ip']         # binary json ==> dict ==> string
         except:
             continue
     return IP
@@ -23,11 +23,11 @@ def create_app(IP=get_ip()):
     cache.init_app(app)
 
     @app.route("/")
-    @cache.cached(timeout=30)
+    @cache.cached(timeout=30)               # saved in cache for 30 seconds
     def index(IP=IP):
         return f"<h1>Your current IP is: {IP}</h1>"
     
-    app.run(host='127.0.0.1', debug=True)
+    app.run(host='127.0.0.1', debug=False)
 
 if __name__ == '__main__':
     create_app()
